65661151c53a76157821c9c80e66f6938f1e79a1
Luke Gessler <ldg3fa@virginia.edu>
2019-04-18 16:06:03 -0400

    fixed

--- src/main.py
+++ src/main.py
@@ -74,7 +74,7 @@ def one_hot_encode_label(label, labels):
     return np.array(vec)
 
 
-def batch_generator(data, labels, vocab, batch_size=1):
+def batch_generator(data, labels, vocab, labels_vocab, batch_size=1):
     while True:
         batch_x = []
         batch_y = []
@@ -82,11 +82,12 @@ def batch_generator(data, labels, vocab, batch_size=1):
             word = START + word + END
             norm_word = START + norm_word + END
             batch_x.append(vectorize_sequence(word, vocab))
-            batch_y.append([one_hot_encode_label(label, vocab) for label in norm_word])
+            batch_y.append([one_hot_encode_label(label, labels_vocab) for label in norm_word])
             if len(batch_x) >= batch_size:
                 # Pad Sequences in batch to same length
-                batch_x = pad_sequences(batch_x, vocab[PAD])
-                batch_y = pad_sequences(batch_y, one_hot_encode_label(PAD, vocab))
+                pad_length = len(max(batch_x + batch_y, key=lambda x: len(x)))
+                batch_x = pad_sequences(batch_x, pad_length, vocab[PAD])
+                batch_y = pad_sequences(batch_y, pad_length, one_hot_encode_label(PAD, labels_vocab))
                 yield np.array(batch_x), np.array(batch_y)
                 batch_x = []
                 batch_y = []
@@ -105,13 +106,12 @@ def describe_data(data, gold_labels, label_set, generator):
     print('Batch output shape:', batch_y.shape)
 
 
-def pad_sequences(batch_x, pad_value):
+def pad_sequences(batch_x, pad_length, pad_value):
     ''' This function should take a batch of sequences of different lengths
         and pad them with the pad_value token so that they are all the same length.
 
         Assume that batch_x is a list of lists.
     '''
-    pad_length = len(max(batch_x, key=lambda x: len(x)))
     for i, x in enumerate(batch_x):
         if len(x) < pad_length:
             batch_x[i] = x + ([pad_value] * (pad_length - len(x)))
@@ -128,9 +128,9 @@ def transform_text_sequence(seq):
     return seq
 
 
-def make_model(vocab, labels, args):
+def make_model(vocab, labels_vocab, args):
     num_chars = len(vocab.keys())
-    num_labels = len(set(char for char_seq in labels for char in char_seq))
+    num_labels = len(labels_vocab.keys())
 
     model = Sequential()
     model.add(Embedding(num_chars, args.embedding_size))
@@ -148,8 +148,8 @@ def make_model(vocab, labels, args):
     return model
 
 
-def eval_model(model, data, labels, vocab, args):
-    loss, acc = model.evaluate_generator(batch_generator(data, labels, vocab, args.batch_size), steps=len(data))
+def eval_model(model, data, labels, vocab, labels_vocab, args):
+    loss, acc = model.evaluate_generator(batch_generator(data, labels, vocab, labels_vocab, args.batch_size), steps=len(data))
     print('Loss:', loss, 'Acc:', acc)
 
 
@@ -159,16 +159,26 @@ def main(args):
     test_data, test_labels = retrieve_all_data()
 
     vocab = {c: i for i, c in enumerate(list(set(c for w in vocab for c in w)) + [UNK, PAD, START, END])}
+    labels_vocab = {c: i for i, c in
+                    enumerate(list(set(c for w in train_labels + dev_labels + test_labels for c in w))
+                              + [UNK, PAD, START, END])}
 
-    describe_data(train_data, train_labels, vocab, batch_generator(train_data, train_labels, vocab, args.batch_size))
+    describe_data(train_data, train_labels, vocab, batch_generator(train_data, train_labels, vocab, labels_vocab, args.batch_size))
 
-    model = make_model(vocab, train_labels, args)
+    model = make_model(vocab, labels_vocab, args)
+
+    # training
+    model.fit_generator(batch_generator(train_data, train_labels, vocab, labels_vocab, args.batch_size),
+                        epochs=args.epochs,
+                        steps_per_epoch=len(train_data) / args.batch_size,
+                        callbacks=[EarlyStopping(monitor="acc", patience=2)])
 
     # Evaluation
-    eval_model(model, train_data, train_labels, vocab, args)
+    eval_model(model, train_data, train_labels, vocab, labels_vocab, args)
 
 
 if __name__ == '__main__':
     args = argparser.parse_args()
     print("Arguments: " + str(args))
     main(args)
+
